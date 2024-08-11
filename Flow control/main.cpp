50f0ab2e6378e514039c0962aaabd0a52f5571ae
waliulohan <waliulislam1742@gmail.com>
2024-08-11 17:52:54 +0600

    flow controll

--- Flow control/main.cpp
+++ Flow control/main.cpp
@@ -1,102 +1,94 @@
 #include <iostream>
-#include <string>
-#include <limits> 
+// flow control
 
-#include <iomanip>
-#include <ios>
-
-
-int addNumbers(int a  ,int b){
-    return a + b;
-}
-int theBasicStaffs (){
-    std::cout << "hello world" << std::endl;    
-
-    std::cerr << "my custom error" << std::endl;    
-
-    // Taking input
-    int age;
-    std::cout << "Please enter your age :" << std::endl;
-    std::cin >> age;
-
-    std::cout << "You entered: " << age << std::endl;
-
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-    std::string name;
-    std::cout << "Please enter your name :" << std::endl;
-    std::getline(std::cin, name); 
-    std::cout << "You entered: " << name << std::endl;
+int if_else(){
+    int a = 5;
+    int b = 10;
 
 
-    // using function
-
-    int res = addNumbers(10, 5);
-
-    std::cout << res << std::endl;
-
-    return 0;
-
-}
-
-int dataTyes (){
-    // integers and float and double
-    int num1 {22};
-    float num2 {22.346};
-    double num3 {22.346344};
-    long double num4 {22.346344};
+    if(a > b){
+        std::cout << "a is greater that b\n";
+    }else{
+        std::cout << "a is not greater that b\n";
+    }
+    std::cout <<   "--------------------------------" << std::endl;
+    std::cout << "making a=20 \n";
 
 
-    std::cout << "size of int   " << num1 << " is  " <<sizeof(num1) << std::endl;
-    std::cout << "size of float  "<< num2 << "  is  "  <<sizeof(num2) << std::endl;
-    std::cout << "size of double  " << num3 << "  is  " <<sizeof(num3) << std::endl;
-    std::cout << "size of long double  "<< num3 << "  is  "  <<sizeof(num4) << std::endl;
-    
-    // boolean
-    bool red_flag {false};
-    std::cout << std::boolalpha;   // this will print red_flag as "false" not 0
-    std::cout << "size of bool  "<< red_flag << "  is  "  <<sizeof(red_flag) << std::endl;
-    
-    // character
-    char char1 {'a'};
-    std::cout << "size of char  "<< char1 << "  is  "  <<sizeof(char1) << std::endl;
+    a = 20;
+
+    if(a > b){
+        std::cout << "a is greater that b\n";
+    }else{
+        std::cout << "a is not greater that b\n";
+    }
 
 
     return 0;
 }
+ void age_switch(int age){
+  
+        switch(age){
+                case 0:{
+                    std::cout << "you are 0 years old" << std::endl;
+                }
+                break;
+                case 10:{
+                    std::cout << "you are 10 years old" << std::endl;
+                }
+                break;
+                case 20:{
+                    std::cout << "you are 20 years old" << std::endl;
+                }
+                break;
+                case 30:{
+                    std::cout << "you are 30 years old" << std::endl;
+                }
+                break;
+                default:{
+                    std::cout << "your age doesn't match our condition" << std::endl;
+                }
+                break;
+            }
+    }
 
 
-int outputFormatting(){
-    std::cout << "hello world" << std::endl;
-    std::cout << "hello world" << std::flush;
+int trying_switch(){
 
-    std::cout << std::setw(50) << "hello world" << std::endl;
-    std::cout << std::setw(50) << std::setfill('-') << "hello world" << std::endl;
-    std::cout << std::setw(50) << std::setfill(' ') << std::right << "hello world" << std::endl;
-    std::cout << std::setw(50) << std::left << "hello world" << std::endl;
-    
-    std::cout << std::uppercase; // this wil make x393484 to X393484
-    std::cout << std::hex << 28 << std::endl;
-    std::cout << std::nouppercase;
-    std::cout << std::hex << 28 << std::endl;
+    int my_age{0};
+    age_switch(my_age);
 
+    std::cout << "---------------" << std::endl;
+    my_age=10;
+    age_switch(my_age);
 
+    std::cout << "---------------" << std::endl;
+    my_age=20;
+    age_switch(my_age);
+
+    std::cout << "---------------" << std::endl;
+    my_age=30;
+    age_switch(my_age);
+
+    std::cout << "---------------" << std::endl;
+    my_age=40;
+    age_switch(my_age);
 
     return 0;
 }
 
+int main(){
+    // if_else();
+    // trying_switch();
 
-int main() { 
-    //theBasicStaffs();
-    //  dataTyes();  
-    outputFormatting();
+    // ternary Operator
+
+    int x = 20;
+    int y = 40;
+
+    int z = (x > y) ? 99 : 44 ;
+    // condition didn't matched so value of z will be 44
+    std::cout << z << std::endl;
 
     return 0;
 }
-
-
-
-/*
-Yes, you can use comments
-Even multiline comments
-*/
