1c812a1a5ae4d9bba7fdb358c140fea5ddd6a9e9
waliulohan <waliulislam1742@gmail.com>
2024-08-23 12:19:59 +0600

    String

---
+++ string/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    // I am going to learn sttring here.
+
+    string first_name = "Walul" ; // declaring a string
+
+    string last_name = "Rohan";
+
+    string full_name = first_name + " " + last_name;
+
+    cout << full_name << endl ;
+
+    return 0;
+}
